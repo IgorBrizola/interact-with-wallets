95fdab169421ba6fe302e939bb03727c74a62d0e
IgorBrizola <igorbrizzola@gmail.com>
2024-10-28 18:51:59 -0300

    chore: adicionando useMemo
    consegue detectar uma wallet com rede solana

--- src/components/WalletContextProvider.tsx
+++ src/components/WalletContextProvider.tsx
@@ -1,14 +1,13 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useMemo } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import * as web3 from '@solana/web3.js'
-import * as walletAdapterWallets from '@solana/wallet-adapter-wallets';
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 require('@solana/wallet-adapter-react-ui/styles.css');
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const endpoint = web3.clusterApiUrl('devnet')
-	const wallets = [new walletAdapterWallets.PhantomWalletAdapter()]
+	const wallets = useMemo(()=> [],[]) 
 
 	return (
 		<ConnectionProvider endpoint={endpoint}>
