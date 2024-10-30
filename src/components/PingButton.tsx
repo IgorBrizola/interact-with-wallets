90b7af09329aba70d69a6068716527e6ee09fbb4
IgorBrizola <igorbrizzola@gmail.com>
2024-10-30 09:28:47 -0300

    chore: ajustando button ping

--- src/components/PingButton.tsx
+++ src/components/PingButton.tsx
@@ -1,6 +1,6 @@
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 import * as web3 from '@solana/web3.js';
-import { FC, useState } from 'react';
+import React, { FC, useState } from 'react';
 import Link from 'next/link';
 
 const PROGRAM_ID = `ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa`;
@@ -49,24 +49,30 @@ export const PingButton: FC = () => {
         console.error('Transação expirou, tentando novamente...');
     } else {
         console.error('Erro ao enviar a transação:', error);
-    }}}
+    }}
+    }  
 
     return (
-        <div className="flex flex-col items-center justify-between h-30">
-            <button  onClick={onClick}
-  className="cursor-pointer transistion-all w-40 justify-center duration-500 hover:shadow-[0_15px_50px_-15px_#13b6da] p-[12px] rounded-[24px] flex bg-gradient-to-r from-indigo-400 to-[#13b6da]"
->
-  <span className="text-[1.9rem] font-bold text-white pr-3 uppercase">Ping!</span>
-</button>
-
-<span className='mt-12'>
-    {signa && (
-        <Link href={signa} target="_blank" rel="noopener noreferrer" className="bg-gradient-to-t from-purple-500 to-teal-300 bg-clip-text text-transparent text-4xl font-bold hover:text-emerald-400 text-decoration-line: underline"
->
-            Solana Explorer
-        </Link>
-    )}
-</span>
-        </div>
+        <>
+            <div className="flex flex-col items-center justify-between h-30">
+                {
+                    publicKey && (
+                        <button onClick={onClick}
+                            className="cursor-pointer transistion-all w-52 justify-center duration-500 hover:shadow-[0_15px_50px_-15px_#13b6da] p-[12px] rounded-[14px] flex bg-gradient-to-r from-purple-500 to-teal-500"
+                        >
+                            <span className="text-[1.9rem] font-light text-white uppercase">Ping!</span>
+                        </button>
+                    )
+                }
+                <span className='mt-12'>
+                    {signa && (
+                        <Link href={signa} target="_blank" rel="noopener noreferrer" className="bg-gradient-to-t from-purple-500 to-teal-300 bg-clip-text text-transparent text-4xl font-normal hover:text-emerald-400 text-decoration-line: underline"
+                        >
+                            Solana Explorer
+                        </Link>
+                    )}
+                </span>
+            </div>
+      </>
     );
 };
